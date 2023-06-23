037d60f38f47e74186fa8e695be46de7afdb9ad7
mvidyasagarreddy <mvsreddy2018@gmail.com>
2023-06-23 18:04:03 +0530

    Adding print script for Hello World

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,3 +1,6 @@
 ls -l
 pwd
-yum list installed | grep vim
+#yum list installed | grep vim
+
+#This is a comment
+
---
+++ 02-print.sh
@@ -0,0 +1,3 @@
+#To print there are widely used commands in bash, but echo is widely used
+
+echo Hello world
